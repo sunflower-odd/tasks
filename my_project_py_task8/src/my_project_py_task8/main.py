b800e0ed031aaed0645cee02afd3c8092a25bd0e
sunflower-odd <sunflower@deeplay.io>
2025-11-13 20:32:18 +0300

    wip

--- my_project_py_task8/src/my_project_py_task8/main.py
+++ my_project_py_task8/src/my_project_py_task8/main.py
@@ -1,9 +1,30 @@
-import re
-import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from .models import CustomerRequest
+import json
+from pathlib import Path
+from datetime import datetime
 
-app = FastAPI()
+app = FastAPI(title="Customer Requests Service")
 
-@app.get("/")
-def add_nums(a, b):
-    return a + b
+DATA_FILE = Path("customers.json")
+
+@app.post("/customer/")
+async def create_customer(request: CustomerRequest):
+    try:
+        record = request.dict()
+        #record["created_at"] = datetime.utcnow().isoformat()
+        record["birth_date"] = record["birth_date"].isoformat()
+
+        if DATA_FILE.exists():
+            with DATA_FILE.open("r+", encoding="utf-8") as f:
+                data = json.load(f)
+                data.append(record)
+                f.seek(0)
+                json.dump(data, f, ensure_ascii=False, indent=4)
+        else:
+            with DATA_FILE.open("w", encoding="utf-8") as f:
+                json.dump([record], f, ensure_ascii=False, indent=4)
+
+        return {"status": "success", "data": record}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- my_project_py_task8/src/my_project_py_task8/models.py
+++ my_project_py_task8/src/my_project_py_task8/models.py
@@ -1,4 +1,22 @@
-from pydantic import BaseModel, Field, validator, EmailStr
+from pydantic import BaseModel, Field, EmailStr, validator
+import re
+from datetime import date
 
-import uvicorn
+class CustomerRequest(BaseModel):
+    last_name: str = Field()
+    first_name: str = Field()
+    birth_date: date
+    phone: str
+    email: EmailStr
 
+    @validator("last_name", "first_name")
+    def name_must_be_cyrillic(cls, v):
+        if not re.fullmatch(r"[А-ЯЁ][а-яё]+", v):
+            raise ValueError("Должно быть с заглавной буквы и содержать только кириллицу")
+        return v
+
+    @validator("phone")
+    def phone_must_be_valid(cls, v):
+        if not re.fullmatch(r"\+?\d{10,15}", v):
+            raise ValueError("Номер телефона должен содержать 10-15 цифр, можно с +")
+        return v

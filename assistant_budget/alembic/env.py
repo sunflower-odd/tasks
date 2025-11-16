389d06b2d5d71564aee3edf5605b8cea0a0dfcc8
sunflower-odd <sunflower@deeplay.io>
2025-11-16 20:04:50 +0300

    wip

---
+++ assistant_budget/alembic/env.py
@@ -0,0 +1,46 @@
+import sys
+import os
+from logging.config import fileConfig
+from sqlalchemy import create_engine, pool
+from alembic import context
+from assistant_budget.db.database import DATABASE_URL, Base
+from assistant_budget.src.models import User, Category, Expense, ExpenseParticipant, Family
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+config = context.config
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+
+def run_migrations_offline():
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    connectable = create_engine(DATABASE_URL)
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata
+        )
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
---
+++ assistant_budget/src/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from assistant_budget.db.database import Base, engine, SessionLocal
+from assistant_budget.src import models
+
+app = FastAPI()
+
+# создаём все таблицы, если их ещё нет
+Base.metadata.create_all(bind=engine)
+
+# dependency для работы с БД
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+def read_root(db: Session = Depends(get_db)):
+    categories = db.query(models.Category).all()
+    return {"categories": [c.name for c in categories]}
